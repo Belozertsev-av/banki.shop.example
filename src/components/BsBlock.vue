d7285bd4afdd4043ad5bbedec39f7d1fe7544fc4
belozertsev_aw <andrew.vl.belozertsev@gmail.com>
2023-04-04 22:46:24 +0300

    added aliases

--- src/components/BsBlock.vue
+++ src/components/BsBlock.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="block" :class="{ unactive : paintItem.isSold === true}">
     <div class="block__img" @click="isOpenModal = true">
-      <img :src="'@img/' + paintItem.img[0]"
+      <img :src="'@/assets/img/' + paintItem.img[0]"
            alt="img">
     </div>
     <div class="block__body">
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,7 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@img': '/src/assets/img',
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   }
 })
